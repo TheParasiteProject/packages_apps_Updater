f80221df6cf41d57196743018f223ef31651d124
someone5678 <someone5678@users.noreply.github.com>
2024-05-05 16:44:21 +0900

    Updater: Generify custom project info
    Change-Id: If327e404f66e16e8034a6c7d914c8455f8baf87c

--- app/src/main/java/org/lineageos/updater/UpdatesActivity.java
+++ app/src/main/java/org/lineageos/updater/UpdatesActivity.java
@@ -178,8 +178,7 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
         }
 
         TextView headerTitle = findViewById(R.id.header_title);
-        headerTitle.setText(getString(R.string.header_title_text,
-                BuildInfoUtils.getBuildVersion()));
+        headerTitle.setText(getString(R.string.header_title_text));
 
         updateLastCheckedString();
 
