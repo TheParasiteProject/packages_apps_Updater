LineageOS/android_packages_apps_Updater

Updater: Fix local update installation progress not showing
When installing a locally imported update, the installation progress
bar was not displayed in the Updater app until the user navigated away
from and back to the activity.

This was caused by a race condition where the RecyclerView was still
processing the item insertion while receiving the first progress update
broadcast, causing the UI state change to be missed.

This commit fixes the issue by restructuring the logic in
onImportCompleted(). The imported update is now added to the adapter and
the UI is refreshed *before* the installation confirmation dialog is
shown.

This decouples the UI update from the action trigger, resolving the
race condition and allowing the progress bar to appear immediately upon
starting the installation.

Change-Id: Ie06903916b5289f8e887f9be471864c45b0e235e

app/src/main/java/org/lineageos/updater/UpdatesActivity.java
@@ -320,6 +320,7 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
 
     @Override
     public void onImportCompleted(Update update) {
+        UpdaterController controller = mUpdaterService.getUpdaterController();
         if (importDialog != null) {
             importDialog.dismiss();
             importDialog = null;
@@ -334,7 +335,21 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
             return;
         }
 
-        mAdapter.notifyDataSetChanged();
+        List<String> updateIds = new ArrayList<>();
+        List<UpdateInfo> sortedUpdates = controller.getUpdates();
+        if (sortedUpdates.isEmpty()) {
+            findViewById(R.id.no_new_updates_view).setVisibility(View.VISIBLE);
+            findViewById(R.id.recycler_view).setVisibility(View.GONE);
+        } else {
+            findViewById(R.id.no_new_updates_view).setVisibility(View.GONE);
+            findViewById(R.id.recycler_view).setVisibility(View.VISIBLE);
+            sortedUpdates.sort((u1, u2) -> Long.compare(u2.getTimestamp(), u1.getTimestamp()));
+            for (UpdateInfo updateInfo : sortedUpdates) {
+                updateIds.add(update.getDownloadId());
+            }
+            mAdapter.setData(updateIds);
+            mAdapter.notifyDataSetChanged();
+        }
 
         final Runnable deleteUpdate = () -> UpdaterController.getInstance(this)
                 .deleteUpdate(update.getDownloadId());
@@ -343,7 +358,9 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
                 .setTitle(R.string.local_update_import)
                 .setMessage(getString(R.string.local_update_import_success, update.getVersion()))
                 .setPositiveButton(R.string.local_update_import_install, (dialog, which) -> {
-                    mAdapter.addItem(update.getDownloadId());
+                    // The item is already in the adapter, just trigger the installation.
+                    // The broadcast receiver will handle updating the UI to show progress.
+
                     // Update UI
                     getUpdatesList();
                     Utils.triggerUpdate(this, update.getDownloadId());

TheParasiteProject/packages_apps_Updater

app/src/main/java/org/lineageos/updater/UpdatesActivity.java
@@ -323,6 +323,7 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
 
     @Override
     public void onImportCompleted(Update update) {
+        UpdaterController controller = mUpdaterService.getUpdaterController();
         if (importDialog != null) {
             importDialog.dismiss();
             importDialog = null;
@@ -337,7 +338,21 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
             return;
         }
 
-        mAdapter.notifyDataSetChanged();
+        List<String> updateIds = new ArrayList<>();
+        List<UpdateInfo> sortedUpdates = controller.getUpdates();
+        if (sortedUpdates.isEmpty()) {
+            findViewById(R.id.no_new_updates_view).setVisibility(View.VISIBLE);
+            findViewById(R.id.recycler_view).setVisibility(View.GONE);
+        } else {
+            findViewById(R.id.no_new_updates_view).setVisibility(View.GONE);
+            findViewById(R.id.recycler_view).setVisibility(View.VISIBLE);
+            sortedUpdates.sort((u1, u2) -> Long.compare(u2.getTimestamp(), u1.getTimestamp()));
+            for (UpdateInfo updateInfo : sortedUpdates) {
+                updateIds.add(updateInfo.getDownloadId());
+            }
+            mAdapter.setData(updateIds);
+            mAdapter.notifyDataSetChanged();
+        }
 
         final Runnable deleteUpdate = () -> UpdaterController.getInstance(this)
                 .deleteUpdate(update.getDownloadId());
@@ -346,7 +361,12 @@ public class UpdatesActivity extends UpdatesListActivity implements UpdateImport
                 .setTitle(R.string.local_update_import)
                 .setMessage(getString(R.string.local_update_import_success, update.getVersion()))
                 .setPositiveButton(R.string.local_update_import_install, (dialog, which) -> {
-                    mAdapter.addItem(update.getDownloadId());
+                    if (controller.isInstallingUpdate()) {
+                        return;
+                    }
+                    // The item is already in the adapter, just trigger the installation.
+                    // The broadcast receiver will handle updating the UI to show progress.
+
                     // Update UI
                     getUpdatesList();
                     Utils.triggerUpdate(this, update.getDownloadId());